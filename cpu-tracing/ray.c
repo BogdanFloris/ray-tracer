2673fb55ccb4016e5c131edcab909f7ecaa14699
Bogdan Floris <bogdan.floris@gmail.com>
2024-05-27 12:04:30 +0300

    chore: add more tests for ray at

--- cpu-tracing/ray.c
+++ cpu-tracing/ray.c
@@ -16,5 +16,13 @@ void test_ray() {
     assert(result.e[0] == 9);
     assert(result.e[1] == 12);
     assert(result.e[2] == 15);
+    ray_at(&result, &r, 0);
+    assert(result.e[0] == 1);
+    assert(result.e[1] == 2);
+    assert(result.e[2] == 3);
+    ray_at(&result, &r, -1);
+    assert(result.e[0] == -3);
+    assert(result.e[1] == -3);
+    assert(result.e[2] == -3);
     printf("ray tests passed!\n");
 }
